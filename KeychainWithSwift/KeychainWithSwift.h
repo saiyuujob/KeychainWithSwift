011a1550a17a734c9e8c85fd4e409c134d114c2e
iosdev <iosdev@iosdevnoMacBook-Pro.local>
2017-07-18 14:05:05 +0900

    Add project

---
+++ KeychainWithSwift/KeychainWithSwift.h
@@ -0,0 +1,19 @@
+//
+//  KeychainWithSwift.h
+//  KeychainWithSwift
+//
+//  Created by iosdev on 2017/07/18.
+//  Copyright © 2017年 iosdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeychainWithSwift.
+FOUNDATION_EXPORT double KeychainWithSwiftVersionNumber;
+
+//! Project version string for KeychainWithSwift.
+FOUNDATION_EXPORT const unsigned char KeychainWithSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeychainWithSwift/PublicHeader.h>
+
+
